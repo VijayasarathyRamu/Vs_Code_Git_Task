1245f8cd16e1221f3e8b7b4db6fa6d3efd7bece9
VijayasarathyRamu <vijayasarathy.ramu@gmail.com>
2025-07-10 12:19:39 +0530

    by Vijay

---
+++ mydata.ts
@@ -0,0 +1,14 @@
+export let myFullName:string = "vijayasarathy";
+
+export class Product{
+    dfgjkdfhg
+
+    constructor(id:number,name:string,price:number){
+        this.pId = id;
+        this.pName = name;
+        this.pPrice = price;
+    }
+    productDetails(){
+        return `Id:${this.pId} Name:${this.pName} Price:${this.pPrice}`
+    }
+}
